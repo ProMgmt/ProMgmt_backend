f14f09ffff9b5fc0e12f15575b871cc5f6a3ac6b
David Kosmos <dakos@Davids-MacBook-Pro.local>
2018-03-08 15:27:46 -0800

    baseline org model

--- model/org.js
+++ model/org.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orgSchema = Schema({
+  name: {type: String, required: true},
+  users: [{type: Schema.Types.ObjectId, ref: 'user'}],
+});
+
+module.exports = mongoose.model('org', orgSchema);
