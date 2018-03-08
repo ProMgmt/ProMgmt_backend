06b9f00faac2e76b09e208d7191db0313bd4ad5c
David Kosmos <dakos@Davids-MacBook-Pro.local>
2018-03-08 15:58:06 -0800

    baseline task model

--- model/task.js
+++ model/task.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = Schema({
+  userId: {type: Schema.Types.ObjectId},
+  projectId: {type: Schema.Types.ObjectId, required: true},
+  orgId: {type: Schema.Types.ObjectId, required: true},
+  adminId: {type: Schema.Types.ObjectId},
+  desc: {type: String, required: true},
+  startDate: {type: Date},
+  dueDate: {type: Date},
+  endDate: {type: Date},
+  estimatedTime: {type: Number},
+  status: {type: String},
+  isDependency: {type: Boolean},
+  dependentTasks: [{type: Schema.Types.ObjectId}],
+});
+
+module.exports = mongoose.model('task', taskSchema);
