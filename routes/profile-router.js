63cb7645cce51c35a08242797744d55de126fa61
Katy Robinson <katyorob@gmail.com>
2018-03-12 11:09:15 -0700

    get route profile done

--- routes/profile-router.js
+++ routes/profile-router.js
@@ -12,13 +12,21 @@ const profileRouter = module.exports = Router();
 profileRouter.post('/api/user/:userId/profile', bearerAuth, jsonParser, function(req, res, next) {
   debug('POST: /api/user/userId/profile');
 
-  //TODO: route logic
+  if(!req.body.userId) {
+    return next(createError(400, 'user id not found'))
+  }
+
+  new Profile(req.body).save()
+    .then( profile => res.json(profile))
+    .catch(next);
 });
 
 profileRouter.get('/api/profile/:profileId', bearerAuth, function(req, res, next) {
   debug('GET: /api/profile');
 
-  //TODO: route logic
+  Profile.findById(req.params.profileId)
+    .then( profile => res.json(profile))
+    .catch(next);
 });
 
 profileRouter.put('/api/profile/:profileId', bearerAuth, jsonParser, function(req, res, next) {
