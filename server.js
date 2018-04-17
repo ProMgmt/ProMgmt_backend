a2a48039d64e536eac30cb10a34f6faf9859220b
Katy Robinson <katyorob@gmail.com>
2018-04-16 17:47:47 -0700

    got token back for google oauth

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const dotenv =  require('dotenv');
 const cors = require('cors');
 const superagent = require('superagent');
+const uuid = require('uuid/v4');
 
 const userRouter = require('./routes/user-router.js');
 const profileRouter = require('./routes/profile-router.js');
@@ -15,6 +16,7 @@ const orgRouter = require('./routes/org-router.js');
 const projectRouter = require('./routes/project-router.js');
 const taskRouter = require('./routes/task-router.js');
 const attachRouter = require('./routes/attach-router.js');
+const User = require('./model/user.js');
 
 const errors = require('./lib/err-middleware.js');
 
@@ -58,14 +60,47 @@ app.get('/oauth/google/code', function(req, res) {
         res.cookie('X-Promgmt-token', 'promgmt token');
         // interact with your db and add them if they dont exist alraeady
 
-        let firstName = `${response.body.given_name}`;
-        let lastName = `${response.body.family_name}`;
-        let picture = `${response.body.picture}`;
-        let email = `${response.body.email}`;
+        let {
+          given_name: firstName, 
+          family_name: lastName, 
+          email, 
+          picture, 
+        } = response.body;
 
-        let redirectURL = `${process.env.CLIENT_URL}?${firstName}&${lastName}&${picture}&${email}`;
+        const firstNameQuery = `firstName=${firstName}`;
+        const lastNameQuery = `lastName=${lastName}`;
+        const emailQuery = `email=${email}`;
+        const pictureQuery = `picture=${picture}`;
+        
+        User.findOne({ email })
+          .then( user => {
+            if (user) {
+              return user.generateToken();
+            } else {
+              return new User({ email, username: email, password: uuid() }).save()
+                
+                .then( user => user.generateFindHash())
+                // .then( user => user.save())
+                .then( user => user.generateToken())
+                .catch(console.log);
 
-        res.redirect(`${redirectURL}`); //this is where we make frontend go back from google oauth to our frontend. now we can interact with our database and get token
+            }
+          }) 
+          .then( token => {
+            const tokenQuery = `token=${token}`;
+
+            console.log('TOKEN!', token);
+            
+            let redirectURL = `${process.env.CLIENT_URL}?${firstNameQuery}&${lastNameQuery}&${pictureQuery}&${emailQuery}&${tokenQuery}`;
+
+            console.log('DIS YER URL', `${redirectURL}`);
+
+            res.redirect(`${redirectURL}`); //this is where we take our app back from google oauth to our frontend. now we can interact with our database and get token
+            
+          })
+          .catch(console.log);
+
+        
       });
   }
 });
@@ -87,3 +122,8 @@ const server = module.exports = app.listen(PORT, () => {
 });
 
 server.isRunning = true;
+
+
+// our url:
+// http://localhost:8080/?firstName=Katy&lastName=Robinson&picture=https://lh5.googleusercontent.com/-PQ_hB_MyqVQ/AAAAAAAAAAI/AAAAAAAAAeQ/nzcMv1zh42Q/photo.jpg?sz=50&email=katelynolearyyy@gmail.com&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjdhNzQyYjIwNzIxOTgwZmI4Mjg1NTI3NmQ0NjQxODE5OWRjMTU4OWZhODBmZDNmZWIwMWRhYjI1NDNmZGIzY2MiLCJpYXQiOjE1MjM5MjU5NDZ9.Zueb_ZCa3NF8cSBzTm77xvW28EgHGA3Qdk2c3i1QKz4#
+//
